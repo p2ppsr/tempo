3b530ffea9e717ed0a199d2e0539c20676039bb6
Brayden Langley <brayden167@gmail.com>
2022-08-05 17:33:09 -0700

    Added docker-compose file

---
+++ src/utils/test.js
@@ -0,0 +1,18 @@
+const parapet = require('parapet-js')
+const test = async () => {
+  return await parapet({
+    resolvers: ['http://localhost:3103'],
+    bridge: '1LQtKKK7c1TN3UcRfsp8SqGjWtzGskze36', // TSP
+    request: {
+      type: 'json-query',
+      query: {
+        v: 3,
+        q: {
+          collection: 'songs',
+          find: {}
+        }
+      }
+    }
+  })
+}
+module.exports = test
