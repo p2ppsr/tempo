798b27b47a5cd3c49270d71471ac712a3ad7f994
Brayden Langley <brayden167@gmail.com>
2022-09-12 12:02:53 -0700

    updated resolvers

--- src/utils/fetchSongs.js
+++ src/utils/fetchSongs.js
@@ -1,9 +1,15 @@
 import parapet from 'parapet-js'
 
 export default async () => {
+  const BRIDGEPORT_RESOLVERS = window.location.host.startsWith('staging')
+    ? ['https://staging-bridgeport.babbage.systems']
+    : window.location.host.startsWith('localhost')
+      ? ['http://localhost:3103']
+      : undefined
+
   // Query tempo bridge
   const availableSongs = await parapet({
-    resolvers: ['http://localhost:3103'],
+    resolvers: BRIDGEPORT_RESOLVERS,
     bridge: '1LQtKKK7c1TN3UcRfsp8SqGjWtzGskze36', // TSP
     request: {
       type: 'json-query',
