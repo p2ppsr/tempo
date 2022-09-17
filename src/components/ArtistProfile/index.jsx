0a90ef5ea29c3471b6ee98d3531630fa050c017d
Brayden Langley <brayden167@gmail.com>
2022-09-16 19:36:14 -0700

    Added basic support for filtering songs by artist

--- src/components/ArtistProfile/index.jsx
+++ src/components/ArtistProfile/index.jsx
@@ -6,14 +6,13 @@ import { Link, useLocation } from 'react-router-dom'
 const ArtistProfile = () => {
   const location = useLocation()
   const { song } = location.state
-  console.log('song ' + song)
 
   const [followStatus, setFollowStatus] = useState('Follow')
   return (
     <div className='ArtistProfile'>
       <img alt='artist' src={ProfilePicture} />
       <div>
-        <h1>{song.artist.name}</h1>
+        <h1>{song.artist}</h1>
         <p className='about'>{song.artist.bio}</p>
         <Link className='button tipBtn' to='#'>TIP ARTIST</Link>
         <button className='button' onClick={() => setFollowStatus('Following')}>{followStatus}</button>
--- src/utils/fetchSongs.js
+++ src/utils/fetchSongs.js
@@ -1,7 +1,7 @@
 import parapet from 'parapet-js'
 import constants from './constants'
 
-export default () => parapet({
+export default (artist) => parapet({
   resolvers: constants.bridgeportResolvers,
   bridge: constants.tempoBridge,
   request: {
@@ -10,7 +10,9 @@ export default () => parapet({
       v: 3,
       q: {
         collection: 'songs',
-        find: {}
+        find: {
+          artist
+        }
       }
     }
   }
