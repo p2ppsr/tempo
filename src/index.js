1ff69a466cc51020a9c56c724ee305dab49322ec
Brayden Langley <brayden167@gmail.com>
2022-10-14 08:57:28 -0700

    Updated fix for Babbage prompt

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import App from './App'
 import BabbagePrompt from '@babbage/react-prompt'
 
 ReactDOM.render(
-  <BabbagePrompt appName='tempo'>
+  <BabbagePrompt 
+      customPrompt
+      appName='Tempo'
+      author='Project Babbage'
+      authorUrl='https://projectbabbage.com'
+      description='Music streaming and publishing platform built with Babbage.'
+      appIcon='/tempoIcon.png'
+      appImages={[
+        '/tempoBG.png'
+      ]}
+    >
     <App />
   </BabbagePrompt>,
   document.getElementById('root')
