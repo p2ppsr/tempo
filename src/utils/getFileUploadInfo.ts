e08223327be957c4c501ea6843f6d6f25bbd645c
Dan Feinstein <otherdaniel@gmail.com>
2024-02-01 12:11:24 -0800

    Fix: Flatten FileList to File[] in utils/getFileUploadInfo.ts

--- src/utils/getFileUploadInfo.ts
+++ src/utils/getFileUploadInfo.ts
@@ -1,125 +1,124 @@
-import { invoice, derivePaymentInfo } from 'nanostore-publisher'
-import { getURLForFile } from 'uhrp-url'
-import { encrypt } from 'cwi-crypto'
-import constants from './constants'
+import { invoice, derivePaymentInfo } from "nanostore-publisher"
+import { getURLForFile } from "uhrp-url"
+import { encrypt } from "cwi-crypto"
+import constants from "./constants"
 
-// Thanks to https://stackoverflow.com/a/22114687 for this (updated for Typescript by DRF)
 function copy(src: ArrayBuffer | ArrayBufferView): ArrayBuffer {
-	// Determine the buffer to be used based on the type of src
-	const srcBuffer = src instanceof ArrayBuffer ? src : src.buffer
-
-	// Create a new ArrayBuffer with the same byte length as the source
-	const dst = new ArrayBuffer(srcBuffer.byteLength)
-
-	// Copy the content from the source buffer to the destination buffer
-	new Uint8Array(dst).set(new Uint8Array(srcBuffer))
-
-	return dst
+  const srcBuffer = src instanceof ArrayBuffer ? src : src.buffer
+  const dst = new ArrayBuffer(srcBuffer.byteLength)
+  new Uint8Array(dst).set(new Uint8Array(srcBuffer))
+  return dst
 }
 
-const RETENTION_PERIOD = 60 * 24 * 365 * 7
+const RETENTION_PERIOD = 60 * 24 * 365 * 7 // 7 years in minutes
 
 interface GetFileUploadInfoParams {
-	selectedArtwork: File | null
-	selectedMusic: File | null
-	retentionPeriod?: number
+  selectedArtwork: File | FileList | null
+  selectedMusic: File | FileList | null
+  retentionPeriod?: number
 }
 
-// TODO: Add support for checking the UHRP hash to see if the data has already been uploaded.
 const getFileUploadInfo = async ({
-	selectedArtwork = null,
-	selectedMusic = null,
-	retentionPeriod = RETENTION_PERIOD
+  selectedArtwork = null,
+  selectedMusic = null,
+  retentionPeriod = RETENTION_PERIOD,
 }: Partial<GetFileUploadInfoParams> = {}) => {
-	// Defauflt values
-	const filesToUpload = []
-	let artworkFileURL = ''
-	let songURL = ''
-	let songDuration = 0
-	let encryptionKey
+  let filesToUpload: File[] = []
+  let artworkFileURL = ""
+  let songURL = ""
+  let songDuration = 0
+  let encryptionKey
 
-	// Check if we have artwork to upload
-	if (selectedArtwork) {
-		// @ts-ignore
-		const artworkBlob = new Blob([selectedArtwork[0]])
-		const artworkData = new Uint8Array(await artworkBlob.arrayBuffer())
-		artworkFileURL = getURLForFile(artworkData)
-		filesToUpload.push(selectedArtwork)
-	}
+  if (selectedArtwork) {
+    // Flatten FileList to File[]
+    const artworkFiles =
+      selectedArtwork instanceof FileList
+        ? Array.from(selectedArtwork)
+        : [selectedArtwork]
+    filesToUpload.push(...artworkFiles)
+  }
 
-	// Check if we have music to upload
-	if (selectedMusic) {
-		// Get the file contents as arrayBuffers
-		// @ts-ignore
-		const musicBlob = new Blob([selectedMusic[0]])
-		const songData = await musicBlob.arrayBuffer()
+  if (selectedMusic) {
+    const musicFiles =
+      selectedMusic instanceof FileList ? selectedMusic : [selectedMusic]
+    const songData = await musicFiles[0].arrayBuffer()
+    const { duration } = await new window.AudioContext().decodeAudioData(
+      copy(songData)
+    )
+    songDuration = Math.ceil(duration)
+    encryptionKey = await window.crypto.subtle.generateKey(
+      {
+        name: "AES-GCM",
+        length: 256,
+      },
+      true,
+      ["encrypt", "decrypt"]
+    )
 
-		// Calculate audio duration
-		const { duration } = await new window.AudioContext().decodeAudioData(copy(songData))
-		songDuration = Math.ceil(duration)
+    const encryptedData = await encrypt(
+      new Uint8Array(songData),
+      encryptionKey,
+      "Uint8Array"
+    )
+    const blob = new Blob([Buffer.from(encryptedData)], {
+      type: "application/octet-stream",
+    })
+    const encryptedFile = new File([blob], "encryptedSong", {
+      type: "application/octet-stream",
+    })
 
-		// Generate an encryption key for the song data
-		encryptionKey = await window.crypto.subtle.generateKey(
-			{
-				name: 'AES-GCM',
-				length: 256
-			},
-			true,
-			['encrypt', 'decrypt']
-		)
+    songURL = getURLForFile(encryptedData)
+    filesToUpload.push(encryptedFile)
+  }
 
-		// Encrypt the song data
-		const encryptedData = await encrypt(new Uint8Array(songData), encryptionKey, 'Uint8Array')
+  const invoices = []
+  const outputs = []
+  for (const fileOrFileList of filesToUpload) {
+    let files: File[] = []
 
-		// Convert the encrypted file for upload with NanoStore
-		const blob = new Blob([Buffer.from(encryptedData)], {
-			type: 'application/octet-stream'
-		})
-		const encryptedFile = new File([blob], 'encryptedSong', {
-			type: 'application/octet-stream'
-		})
+    if (fileOrFileList instanceof FileList) {
+      for (let i = 0; i < fileOrFileList.length; i++) {
+        const fileItem = fileOrFileList.item(i)
+        if (fileItem !== null) {
+          files.push(fileItem)
+        }
+      }
+    } else if (fileOrFileList instanceof File) {
+      // Also check if it's a File instance
+      files.push(fileOrFileList)
+    }
 
-		// Calculate the UHRP addresses, for later use in the TSP script
-		songURL = getURLForFile(encryptedData)
-		filesToUpload.push(encryptedFile)
-	}
+    for (const file of files) {
+      const fileSize = file.size
+      const inv = await invoice({
+        fileSize: fileSize,
+        retentionPeriod,
+        config: { nanostoreURL: constants.nanostoreURL },
+      })
 
-	// Create invoices for hosting any attached song or artwork files on NanoStore
-	const invoices = []
-	const outputs = []
-	for (const file of filesToUpload) {
-		const inv = await invoice({
-			fileSize: file.size,
-			retentionPeriod,
-			config: {
-				nanostoreURL: constants.nanostoreURL
-			}
-		})
+      const paymentInfo = await derivePaymentInfo({
+        recipientPublicKey: inv.identityKey,
+        amount: inv.amount,
+      })
 
-		// Derive the payment info for the given invoice
-		const paymentInfo = await derivePaymentInfo({
-			recipientPublicKey: inv.identityKey,
-			amount: inv.amount
-		})
-		// Save the payment info with the invoice for the later submitPayment call
-		inv.derivationPrefix = paymentInfo.derivationPrefix
-		inv.derivationSuffix = paymentInfo.derivationSuffix
-		inv.derivedPublicKey = paymentInfo.derivedPublicKey
+      inv.derivationPrefix = paymentInfo.derivationPrefix
+      inv.derivationSuffix = paymentInfo.derivationSuffix
+      inv.derivedPublicKey = paymentInfo.derivedPublicKey
 
-		// Add the new invoice and the new transaction output
-		outputs.push(paymentInfo.output)
-		invoices.push(inv)
-	}
+      outputs.push(paymentInfo.output)
+      invoices.push(inv)
+    }
+  }
 
-	return {
-		invoices,
-		outputs,
-		songURL,
-		artworkFileURL,
-		filesToUpload,
-		encryptionKey,
-		songDuration
-	}
+  return {
+    invoices,
+    outputs,
+    songURL,
+    artworkFileURL,
+    filesToUpload,
+    encryptionKey,
+    songDuration,
+  }
 }
 
 export default getFileUploadInfo
