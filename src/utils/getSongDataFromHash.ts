b1b90c33ba1a547b9dfe29e11a38b30a9e17e835
Dan Feinstein <otherdaniel@gmail.com>
2024-03-30 22:03:15 -0700

    Update getSongDataFromHash.ts

--- src/utils/getSongDataFromHash.ts
+++ src/utils/getSongDataFromHash.ts
@@ -1,25 +1,15 @@
-import { Song } from "../types/interfaces"
-import fetchSongs from "./fetchSongs/fetchSongs"
+import { Song } from '../types/interfaces'
+import fetchSongs from './fetchSongs/fetchSongs'
 
 export const getSongDataFromHash = async (songURL: string): Promise<Song> => {
-<<<<<<< Updated upstream
-
-  const res = await fetchSongs({
-=======
   console.log(`called getSongFromHash with songURL: `, songURL)
 
   const encodedSongURL = Buffer.from(songURL).toString('base64')
 
   const response = await fetchSongs({
->>>>>>> Stashed changes
     findAll: true,
     songIDs: [encodedSongURL]
   })
 
-<<<<<<< Updated upstream
-  return res[0]
-}
-=======
   return response[0]
 }
->>>>>>> Stashed changes
