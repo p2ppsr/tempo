a89dc27ca4879469efab9fa7ece89e8d29e65576
Dan Feinstein <otherdaniel@gmail.com>
2024-03-30 21:26:07 -0700

    Update getSongDataFromHash.ts

--- src/utils/getSongDataFromHash.ts
+++ src/utils/getSongDataFromHash.ts
@@ -2,6 +2,7 @@ import { Song } from '../types/interfaces'
 import fetchSongs from './fetchSongs/fetchSongs'
 
 export const getSongDataFromHash = async (songURL: string): Promise<Song> => {
+  console.log('called getsongfromHash with songURL: ', songURL)
   const res = await fetchSongs({
     findAll: true,
     songIDs: [songURL].map((song: string) => {
