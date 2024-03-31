b63fb895428c644646e82c656f4b5c81b94ea5ec
Dan Feinstein <otherdaniel@gmail.com>
2024-03-30 20:08:55 -0700

    Update getSongDataFromHash.ts

--- src/utils/getSongDataFromHash.ts
+++ src/utils/getSongDataFromHash.ts
@@ -1,9 +1,9 @@
-import { Song } from "../types/interfaces"
-import fetchSongs from "./fetchSongs/fetchSongs"
+import { Song } from '../types/interfaces'
+import fetchSongs from './fetchSongs/fetchSongs'
 
 export const getSongDataFromHash = async (songURL: string): Promise<Song> => {
-
   const res = await fetchSongs({
+    true: true,
     findAll: true,
     songIDs: [songURL].map((song: string) => {
       return Buffer.from(song).toString('base64')
