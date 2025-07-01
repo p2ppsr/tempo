150e7c32572c7be6e8cf14260bcbc4f391409c9f
Brayden Langley <brayden167@gmail.com>
2025-07-01 15:36:08 -0700

    Bug fix

--- key-server/scripts/start.sh
+++ key-server/scripts/start.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 if [ "$NODE_ENV" = "production" ] || [ "$NODE_ENV" = "staging" ]; then
-  npm run build
   node dist/src/index.js
-  exit
+else
+  node --inspect=0.0.0.0 dist/src/index.js
 fi
-
-node --inspect=0.0.0.0 dist/src/index.js
